7ae4a3e9c6c39d01edc5e0bdd039472706d9322a
Rokas <rbomblauskas@gmail.com>
2026-06-01 14:09:21 +0100

    show wardrobe screen

--- app/src/main/java/io/github/rbomblauskas/kraitis/MainActivity.kt
+++ app/src/main/java/io/github/rbomblauskas/kraitis/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.rbomblauskas.kraitis.ui.theme.KraitisTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KraitisTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WardrobeApp()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WardrobeApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = { Text("Kraitis") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Text("+")
+            }
+        }
+    ) { innerPadding ->
+        WardrobeScreen(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+private fun WardrobeScreen(modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text("No clothes saved yet")
+            Text("Add the first item to start auditing your wardrobe.")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WardrobeAppPreview() {
     KraitisTheme {
-        Greeting("Android")
+        WardrobeApp()
     }
 }
